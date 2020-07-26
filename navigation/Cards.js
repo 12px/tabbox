d542c4aec8001c3f5a5b1f737f2284b7fbbf2333
Dalton Gore <dalton@12px.io>
2020-07-26 09:46:23 -0600

    Add additional navigation views.

--- navigation/Cards.js
+++ navigation/Cards.js
@@ -3,10 +3,10 @@ import { Text, View } from 'react-native'
 
 import styles from '../constants/styles'
 
-export default function Home() {
+export default function Cards() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home</Text>
+      <Text style={styles.text}>Cards</Text>
     </View>
   );
 }
